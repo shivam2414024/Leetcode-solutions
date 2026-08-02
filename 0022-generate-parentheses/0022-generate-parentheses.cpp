df08b1b3898f9ba3bd349ec387990d87fd162be9
Shivam Purbey <shivampurbey72@gmail.com>
2026-08-02 19:54:56 +0530

    Time: 0 ms (100%), Space: 13 MB (84.09%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     // TC: O(2^2n)  SC: O(2n)
-    bool isValid(string& s) {
-        int count = 0;
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '(')
-                count++;
-            else
-                count--;
-            if (count < 0)
-                return false;
-        }
-        return count == 0;
-    }
-    void solve(string& s, int n, vector<string>& ans) {
+    void solve(string& s, int n, vector<string>& ans, int open, int close) {
         if (s.size() == 2 * n) {
-            if (isValid(s)) {
-                ans.push_back(s);
-            }
+            ans.push_back(s);
             return;
         }
-        s.push_back('(');
-        solve(s, n, ans);
-        s.pop_back();
-        s.push_back(')');
-        solve(s, n, ans);
-        s.pop_back();
+        if (open < n) {
+            s.push_back('(');
+            solve(s, n, ans, open + 1, close);
+            s.pop_back();
+        }
+        if (close < open) {
+            s.push_back(')');
+            solve(s, n, ans, open, close + 1);
+            s.pop_back();
+        }
     }
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
         string s = "";
-        solve(s, n, ans);
+        int open = 0, close = 0;
+        solve(s, n, ans, open, close);
         return ans;
     }
 };
