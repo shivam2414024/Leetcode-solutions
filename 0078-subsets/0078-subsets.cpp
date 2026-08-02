007893208b19a4cbdd02992300f47bf03c903be8
Shivam Purbey <shivampurbey72@gmail.com>
2026-08-02 20:18:44 +0530

    Time: 0 ms (100%), Space: 9.8 MB (96.6%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    // TC: O(2^n) SC:O(n)
+    void solve(vector<int>& nums, int i, vector<int>& temp,
+               vector<vector<int>>& ans) {
+        if (i >= nums.size()) {
+            ans.push_back(temp);
+            return;
+        }
+        temp.push_back(nums[i]);
+        solve(nums, i + 1, temp, ans);
+        temp.pop_back();
+        solve(nums, i + 1, temp, ans);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        solve(nums, 0, temp, ans);
+        return ans;
+    }
+};
