a8477be04041884a6c174cf0df32006ac3e700c8
Shivam Purbey <shivampurbey72@gmail.com>
2026-08-02 19:45:13 +0530

    Time: 3 ms (69.38%), Space: 13.1 MB (84.09%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValid(string& s) {
+        int count = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(')
+                count++;
+            else
+                count--;
+            if (count < 0)
+                return false;
+        }
+        return count == 0;
+    }
+    void solve(string& s, int n, vector<string>& ans) {
+        if (s.size() == 2 * n) {
+            if (isValid(s)) {
+                ans.push_back(s);
+            }
+            return;
+        }
+        s.push_back('(');
+        solve(s, n, ans);
+        s.pop_back();
+        s.push_back(')');
+        solve(s, n, ans);
+        s.pop_back();
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string s = "";
+        solve(s, n, ans);
+        return ans;
+    }
+};
