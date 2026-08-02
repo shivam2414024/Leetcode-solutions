ae3eb2b19ef5f93bfb461b33162c6db749d8a492
Shivam Purbey <shivampurbey72@gmail.com>
2026-08-02 12:09:22 +0530

    Time: 3 ms (61.54%), Space: 12 MB (20.81%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
     // TC: O(N!)  SC: O(N)
-    bool isValid(int n, vector<string>& board, int row, int col){
-        // upward check
-        for(int i=row-1; i>=0; i--){
-            if(board[i][col] == 'Q') return false;
-        }
-
-        // left diagonal check
-        for(int i=row-1, j=col-1; i>=0 && j>=0; i--, j--){
-            if(board[i][j] == 'Q') return false;
-        }
-
-        // right diagonal check
-        for(int i=row-1, j=col+1; i>=0 && j<n; i--, j++){
-            if(board[i][j] == 'Q') return false;
-        }
-
-        return true;
-    }
-    void solve(int n, vector<string>& board, int row, vector<vector<string>>& ans){
-        if(row >= n){
+    void solve(int n, vector<string>& board, int row, unordered_set<int>& cols,
+               unordered_set<int>& diag, unordered_set<int>& antiDiag,
+               vector<vector<string>>& ans) {
+        if (row >= n) {
             ans.push_back(board);
             return;
         }
-        for(int col=0; col < n; col++){
-            if(isValid(n, board, row, col)){
-                board[row][col] = 'Q';
-                solve(n, board, row+1, ans);
-                board[row][col] = '.';
+        for (int col = 0; col < n; col++) {
+            int diagConst = row - col;
+            int antiDiagConst = row + col;
+
+            if (cols.find(col) != cols.end() ||
+                diag.find(diagConst) != diag.end() ||
+                antiDiag.find(antiDiagConst) != antiDiag.end()) {
+                continue;
             }
+
+            cols.insert(col);
+            diag.insert(diagConst);
+            antiDiag.insert(antiDiagConst);
+
+            board[row][col] = 'Q';
+
+            solve(n, board, row + 1, cols, diag, antiDiag, ans);
+
+            cols.erase(col);
+            diag.erase(diagConst);
+            antiDiag.erase(antiDiagConst);
+
+            board[row][col] = '.';
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<string> board(n, string(n, '.'));
+        unordered_set<int> cols, diag, antiDiag;
         vector<vector<string>> ans;
-        solve(n, board, 0, ans);
+        solve(n, board, 0, cols, diag, antiDiag, ans);
         return ans;
     }
 };
