4ff1d65192d643f4bb422f0354cb114a75a873ae
Shivam Purbey <shivampurbey72@gmail.com>
2026-08-05 14:48:45 +0530

    Time: 391 ms (30.69%), Space: 345.9 MB (28.28%) - LeetHub

--- 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -1,54 +1,41 @@
 class Solution {
 public:
     void dfs(int k, unordered_map<int, vector<int>>& adj, vector<bool>& vis,
-             set<int>& st) {
-        st.insert(k);
+             vector<int>& inDegree) {
         vis[k] = true;
         for (auto& it : adj[k]) {
+            inDegree[it]--;
             if (!vis[it]) {
-                dfs(it, adj, vis, st);
+                dfs(it, adj, vis, inDegree);
             }
-        }
-    }
-    void check(int node, unordered_map<int, vector<int>>& adj,
-               vector<bool>& vis, vector<int>& ans, set<int>& st) {
-        vis[node] = true;
-        for (auto& it : adj[node]) {
-            if (!vis[it]) {
-                check(it, adj, vis, ans, st);
-                ans.push_back(it);
-            }
-            if (st.find(it) != st.end())
-                ans.push_back(it);
         }
     }
     vector<int> remainingMethods(int n, int k,
                                  vector<vector<int>>& invocations) {
         unordered_map<int, vector<int>> adj;
+        vector<bool> vis(n, false);
+        vector<int> inDegree(n, 0);
         for (auto& it : invocations) {
             adj[it[0]].push_back(it[1]);
+            inDegree[it[1]]++;
         }
-        vector<bool> vis(n, false);
-        set<int> st;
+        dfs(k, adj, vis, inDegree);
         vector<int> ans;
-        dfs(k, adj, vis, st);
+        bool flag = false;
         for (int i = 0; i < n; i++) {
+            if (vis[i] && inDegree[i] != 0) {
+                flag = true;
+            }
             if (!vis[i]) {
                 ans.push_back(i);
-                check(i, adj, vis, ans, st);
-            }
-        }
-        bool flag = false;
-        for (int i = 0; i < ans.size(); i++) {
-            if (st.find(ans[i]) != st.end()) {
-                flag = true;
-                break;
             }
         }
         if (flag) {
-            ans.clear();
-            for (int i = 0; i < n; i++)
-                ans.push_back(i);
+            vector<int> nums(n);
+            for (int i = 0; i < n; i++) {
+                nums[i] = i;
+            }
+            return nums;
         }
         return ans;
     }
