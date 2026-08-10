e871571139fc1f8ea41cd4edbffdd1c45a10a549
Shivam Purbey <shivampurbey72@gmail.com>
2026-08-10 20:41:54 +0530

    Time: 146 ms (27.29%), Space: 123 MB (25.03%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     int minReorder(int n, vector<vector<int>>& connections) {
-        unordered_map<int, vector<int>> adj;
+        unordered_map<int, vector<pair<int, int>>> adj;
         for (auto& it : connections) {
             int u = it[0];
             int v = it[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            adj[u].push_back({v, 1});
+            adj[v].push_back({u, 0});
         }
-        set<pair<int, int>> st;
         queue<int> q;
         vector<bool> vis(n, false);
         q.push(0);
         vis[0] = true;
+        int count = 0;
         while (!q.empty()) {
             int node = q.front();
             q.pop();
             for (auto& it : adj[node]) {
-                if (!vis[it]) {
-                    vis[it] = true;
-                    auto pair = make_pair(node, it);
-                    st.insert(pair);
-                    q.push(it);
+                int adjNode = it.first;
+                int real = it.second;
+                if (!vis[adjNode]) {
+                    q.push(adjNode);
+                    count += real;
+                    vis[adjNode] = true;
                 }
             }
-        }
-        int count = 0;
-        for (auto& it : connections) {
-            auto P = make_pair(it[0], it[1]);
-            if (st.find(P) != st.end())
-                count++;
         }
         return count;
     }
