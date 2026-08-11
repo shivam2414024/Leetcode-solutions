5a6509f9f95c939551e3243e730343ca3e612ca6
Shivam Purbey <shivampurbey72@gmail.com>
2026-08-11 12:21:19 +0530

    Time: 0 ms (100%), Space: 23 MB (30.18%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
-    bool check(vector<int>& nums, int count, int i, int dir) {
-        int idx = i;
-        vector<int> temp = nums;
-        while (idx >= 0 && idx < nums.size() && count > 0) {
-            if (temp[idx] > 0) {
-                temp[idx]--;
-                dir *= -1;
-                if (temp[idx] == 0)
-                    count--;
-            }
-            idx += dir;
-        }
-        return count == 0;
-    }
     int countValidSelections(vector<int>& nums) {
         int n = nums.size();
         int ans = 0;
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0)
-                count++;
+        vector<int> pref(n, 0);
+        vector<int> suf(n, 0);
+        pref[0] = nums[0];
+        suf[n - 1] = nums[n - 1];
+        for (int i = 1; i < n; i++) {
+            pref[i] = nums[i] + pref[i - 1];
+            suf[n - 1 - i] = nums[n - 1 - i] + suf[n - 1 - i + 1];
         }
         for (int i = 0; i < n; i++) {
             if (nums[i] == 0) {
-                if (check(nums, count, i, -1))
-                    ans++;
-                if (check(nums, count, i, 1))
-                    ans++;
+                if (pref[i] == suf[i])
+                    ans += 2;
+                else if (abs(pref[i] - suf[i]) == 1)
+                    ans += 1;
             }
         }
         return ans;
