cac03575de971f03ec29c267f6ad84d51cd5bb61
Shivam Purbey <shivampurbey72@gmail.com>
2026-08-12 17:53:54 +0530

    Time: 0 ms (100%), Space: 68.1 MB (5.15%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,32 +2,31 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> left(n), right(n);
-        left[0] = 1;
-        right[n - 1] = 1;
-        int count = 1;
-        for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                count++;
-                left[i] = count;
-            } else {
-                count = 1;
-                left[i] = count;
+        int ans = 1;
+        int i = 1;
+        while (i < n) {
+            if (ratings[i] == ratings[i - 1]) {
+                ans++;
+                i++;
+                continue;
+            }
+            int peak = 1;
+            while (i < n && ratings[i] > ratings[i - 1]) {
+                peak++;
+                ans += peak;
+                i++;
+            }
+            int down = 1;
+            while (i < n && ratings[i] < ratings[i - 1]) {
+                ans += down;
+                down++;
+                i++;
+            }
+            if (down > peak) {
+                ans += (down - peak);
             }
         }
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                count++;
-                right[i] = count;
-            } else {
-                count = 1;
-                right[i] = count;
-            }
-        }
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans += max(left[i], right[i]);
-        }
+
         return ans;
     }
 };
