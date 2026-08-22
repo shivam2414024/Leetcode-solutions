17aa3dbeeb12622410e21ad3fbe1abdf68580da3
Shivam Purbey <shivampurbey72@gmail.com>
2026-08-22 20:53:29 +0530

    Time: 1 ms (40.5%), Space: 14.1 MB (54.53%) - LeetHub

--- 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.cpp
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.cpp
@@ -2,41 +2,41 @@ class Solution {
 public:
     typedef long long ll;
     int MOD = 1e9 + 7;
-    pair<ll, ll> solve(int m, int n, int i, int j, vector<vector<int>>& grid,
-                       vector<vector<pair<ll, ll>>>& dp) {
-        if (i == m - 1 && j == n - 1)
-            return {grid[i][j], grid[i][j]};
-
-        if (dp[i][j] != make_pair(LLONG_MIN, LLONG_MAX))
-            return dp[i][j];
-
-        ll maxVal = LLONG_MIN;
-        ll minVal = LLONG_MAX;
-
-        if (i + 1 < m) {
-            auto [downMax, downMin] = solve(m, n, i + 1, j, grid, dp);
-            maxVal = max({maxVal, grid[i][j] * downMax, grid[i][j] * downMin});
-            minVal = min({minVal, grid[i][j] * downMax, grid[i][j] * downMin});
-        }
-
-        if (j + 1 < n) {
-            auto [rightMax, rightMin] = solve(m, n, i, j + 1, grid, dp);
-            maxVal =
-                max({maxVal, grid[i][j] * rightMax, grid[i][j] * rightMin});
-            minVal =
-                min({minVal, grid[i][j] * rightMax, grid[i][j] * rightMin});
-        }
-
-        return dp[i][j] = {maxVal, minVal};
-    }
     int maxProductPath(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<pair<ll, ll>>> dp(
-            m, vector<pair<ll, ll>>(n, {LLONG_MIN, LLONG_MAX}));
+        vector<vector<pair<ll, ll>>> dp(m, vector<pair<ll, ll>>(n));
 
-        auto [maxProd, minProd] = solve(m, n, 0, 0, grid, dp);
+        dp[0][0] = make_pair(grid[0][0], grid[0][0]);
+
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = make_pair(grid[i][0] * dp[i - 1][0].first,
+                                 grid[i][0] * dp[i - 1][0].second);
+        }
+
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = make_pair(grid[0][j] * dp[0][j - 1].first,
+                                 grid[0][j] * dp[0][j - 1].second);
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                long long upMax = dp[i - 1][j].first;
+                long long upMin = dp[i - 1][j].second;
+                long long leftMax = dp[i][j - 1].first;
+                long long leftMin = dp[i][j - 1].second;
+
+                dp[i][j].first =
+                    max({grid[i][j] * upMax, grid[i][j] * upMin,
+                         grid[i][j] * leftMax, grid[i][j] * leftMin});
+                dp[i][j].second =
+                    min({grid[i][j] * upMax, grid[i][j] * upMin,
+                         grid[i][j] * leftMax, grid[i][j] * leftMin});
+            }
+        }
+
+        ll maxProd = dp[m - 1][n - 1].first;
 
         return maxProd < 0 ? -1 : maxProd % MOD;
     }
