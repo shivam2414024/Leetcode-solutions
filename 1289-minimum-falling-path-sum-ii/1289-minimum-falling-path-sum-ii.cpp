af16f12d2171fbbde0409bf002930a516bc348a8
Shivam Purbey <shivampurbey72@gmail.com>
2026-08-23 16:10:34 +0530

    Time: 4 ms (79.84%), Space: 21.5 MB (49.22%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -2,26 +2,42 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n = grid.size();
-        int minSum = INT_MAX;
         vector<vector<int>> dp(n, vector<int>(n));
+        int nextMinCol1 = -1;
+        int nextMinCol2 = -1;
         for (int l = 0; l < n; l++) {
             dp[n - 1][l] = grid[n - 1][l];
+
+            if (nextMinCol1 == -1 || dp[n - 1][l] <= dp[n - 1][nextMinCol1]) {
+                nextMinCol2 = nextMinCol1;
+                nextMinCol1 = l;
+            } else if (nextMinCol2 == -1 ||
+                       dp[n - 1][l] <= dp[n - 1][nextMinCol2]) {
+                nextMinCol2 = l;
+            }
         }
 
         for (int i = n - 2; i >= 0; i--) {
+            int minCol1 = -1;
+            int minCol2 = -1;
             for (int t = 0; t < n; t++) {
-                int mini = 1e9;
-                for (int k = 0; k < n; k++) {
-                    if (t != k)
-                        mini = min(mini, dp[i + 1][k]);
+                if (t != nextMinCol1) {
+                    dp[i][t] = grid[i][t] + dp[i + 1][nextMinCol1];
+                } else {
+                    dp[i][t] = grid[i][t] + dp[i + 1][nextMinCol2];
                 }
-                dp[i][t] = mini + grid[i][t];
+
+                if (minCol1 == -1 || dp[i][t] <= dp[i][minCol1]) {
+                    minCol2 = minCol1;
+                    minCol1 = t;
+                } else if (minCol2 == -1 || dp[i][t] <= dp[i][minCol2]) {
+                    minCol2 = t;
+                }
             }
-        }
-        for (int j = 0; j < n; j++) {
-            minSum = min(minSum, dp[0][j]);
+            nextMinCol1 = minCol1;
+            nextMinCol2 = minCol2;
         }
 
-        return minSum;
+        return dp[0][nextMinCol1];
     }
 };
