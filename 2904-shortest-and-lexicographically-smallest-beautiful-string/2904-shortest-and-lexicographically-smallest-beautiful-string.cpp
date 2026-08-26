db6d4c5491ff0e655a321da946590a95a5c28e8b
Shivam Purbey <shivampurbey72@gmail.com>
2026-08-26 11:48:19 +0530

    Time: 0 ms (100%), Space: 8.6 MB (95.97%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,46 +1,40 @@
 class Solution {
 public:
-    string check(string s1, string s2) {
-        int i = 0, j = 0;
-        while (i < s1.size()) {
-            if (s1[i] != s2[j]) {
-                if (s1[i] < s2[j])
-                    return s1;
-                else if (s1[i] > s2[j])
-                    return s2;
-            }
-            i++;
-            j++;
-        }
-        return s1;
-    }
     string shortestBeautifulSubstring(string s, int k) {
         int n = s.size();
-        int minLen = 0;
         string ans = "";
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = i; j < n; j++) {
-                if (s[j] == '1')
-                    count++;
-                if (count == k) {
-                    if (k == 1)
-                        return "1";
-                    if (minLen == 0) {
-                        minLen = j - i + 1;
-                        ans = s.substr(i, minLen);
-                        break;
-                    } else {
-                        if (minLen > j - i + 1) {
-                            minLen = j - i + 1;
-                            ans = s.substr(i, minLen);
-                        } else if (minLen == j - i + 1) {
-                            ans = check(ans, s.substr(i, j - i + 1));
-                        }
-                        break;
-                    }
-                }
+        int minLen = INT_MAX;
+        int count = 0;
+        int l = 0, r = 0;
+        while (r < n) {
+            if (s[r] == '1')
+                count++;
+
+            while (count > k) {
+                if (s[l] == '1')
+                    count--;
+                l++;
             }
+
+            if (count == k) {
+
+                while (l < r && s[l] == '0')
+                    l++;
+
+                int len = r - l + 1;
+                string curr = s.substr(l, len);
+
+                if (len < minLen) {
+                    minLen = len;
+                    ans = curr;
+                }
+
+                else if (len == minLen && ans > curr)
+                    ans = curr;
+            }
+
+
+            r++;
         }
         return ans;
     }
