f1e8f8a42730fb5d34d047203b413c0854bcf974
Shivam Purbey <shivampurbey72@gmail.com>
2026-09-25 23:38:32 +0530

    Time: 0 ms (100%), Space: 8.3 MB (53.58%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            if (s[i] >= 65 && s[i] <= 90)
+                s[i] = (s[i] + 32);
+        }
+        return s;
+    }
+};
